f528b6d52624882bccb508c89988aeb18f84341d
RezaRiri <rezaririjourney@gmail.com>
2024-03-25 21:56:13 +0700

    update response

--- html/app/Http/Controllers/GoersController.php
+++ html/app/Http/Controllers/GoersController.php
@@ -38,57 +38,70 @@ class GoersController extends Controller
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $response = curl_exec($ch);
+        if ($response == '0') {
+            $data = [
+                'kode' => http_response_code(400),
+                'status' => 'terlalu banyak request'
+            ];
+        } else {
+            $result = json_decode($response);
+            foreach ($result->data->values as $key => $value) {
+                $order_number = $value[0];
+                $item_content = $value[1];
+                $item_group = $value[2];
+                $item_name = $value[3];
+                $item_total = $value[4];
+                $item_price = $value[5];
+                $item_total_price = $value[6];
+                $item_total_tax = $value[7];
+                $item_total_price_before_tax = $value[8];
+                $order_by = $value[9];
+                $status = $value[10];
+                $schedule_date = $value[11];
+                $schedule_datetime = $value[12];
+                $created_at = substr($value[13], 0, 19);
+                $payment_method = $value[14];
 
-        $result = json_decode($response);
+                $find = DB::table('goers')
+                    ->where('order_number', '=', $order_number)
+                    ->get();
 
-        curl_close($ch);
+                $objJsonDocument = json_encode($find);
+                $arrOutput = json_decode($objJsonDocument, TRUE);
 
-        foreach ($result->data->values as $key => $value) {
-            $order_number = $value[0];
-            $item_content = $value[1];
-            $item_group = $value[2];
-            $item_name = $value[3];
-            $item_total = $value[4];
-            $item_price = $value[5];
-            $item_total_price = $value[6];
-            $item_total_tax = $value[7];
-            $item_total_price_before_tax = $value[8];
-            $order_by = $value[9];
-            $status = $value[10];
-            $schedule_date = $value[11];
-            $schedule_datetime = $value[12];
-            $created_at = substr($value[13], 0, 19);
-            $payment_method = $value[14];
+                if (!empty($arrOutput)) {
+                    $data = [
+                        'kode' => http_response_code(200),
+                        'status' => 'data sudah ada'
+                    ];
+                } else {
+                    DB::table('goers')->insert([
+                        'order_number' => $order_number,
+                        'item_content' => $item_content,
+                        'item_group' => $item_group,
+                        'item_name' => $item_name,
+                        'item_total' => $item_total,
+                        'item_price' => $item_price,
+                        'item_total_price' => $item_total_price,
+                        'item_total_tax' => $item_total_tax,
+                        'item_total_price_before_tax' => $item_total_price_before_tax,
+                        'order_by' => $order_by,
+                        'status' => $status,
+                        'schedule_date' => $schedule_date,
+                        'schedule_datetime' => $schedule_datetime,
+                        'created_at' => $created_at,
+                        'payment_method' => $payment_method
+                    ]);
 
-            $find = DB::table('goers')
-                ->where('order_number', '=', $order_number)
-                ->get();
-
-            $objJsonDocument = json_encode($find);
-            $arrOutput = json_decode($objJsonDocument, TRUE);
-
-            if (!empty($arrOutput)) {
-                echo 'data sudah ada';
-            } else {
-                DB::table('goers')->insert([
-                    'order_number' => $order_number,
-                    'item_content' => $item_content,
-                    'item_group' => $item_group,
-                    'item_name' => $item_name,
-                    'item_total' => $item_total,
-                    'item_price' => $item_price,
-                    'item_total_price' => $item_total_price,
-                    'item_total_tax' => $item_total_tax,
-                    'item_total_price_before_tax' => $item_total_price_before_tax,
-                    'order_by' => $order_by,
-                    'status' => $status,
-                    'schedule_date' => $schedule_date,
-                    'schedule_datetime' => $schedule_datetime,
-                    'created_at' => $created_at,
-                    'payment_method' => $payment_method
-                ]);
-                echo http_response_code(200);
+                    $data = [
+                        'kode' => http_response_code(200),
+                        'status' => 'berhasil insert'
+                    ];
+                }
             }
+
+            return $data;
         }
+        curl_close($ch);
     }
 }
